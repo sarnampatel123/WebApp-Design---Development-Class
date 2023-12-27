37833c040679ce9cc891720688846aa54407a7d8
Sarnam <spate247@uncc.edu>
2023-12-26 20:43:27 -0500

    Adding files to Github repo

---
+++ itis3135/brand/scripts/midterm.js
@@ -0,0 +1,8 @@
+function showInput() {
+    document.getElementById('display').innerHTML = 
+                document.getElementById("name").value;
+    document.getElementById('display').innerHTML = 
+                document.getElementById("email").value;
+    document.getElementById('display').innerHTML = 
+                document.getElementById("phonenumber").value;
+}
